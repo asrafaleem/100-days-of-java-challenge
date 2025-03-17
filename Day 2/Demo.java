7403600763d29f5d8178d7af8a77be07d4b0d298
asrafaleem <asrafaleem8@gmail.com>
2025-03-17 20:59:22 +0530

    Updated files with new changes

---
+++ Day 2/Demo.java
@@ -0,0 +1,27 @@
+public class Demo{
+    public static void main(String[] args) {
+        
+    
+    //variables and data types - primitive 
+    boolean check = true; // 1 bit 
+    char letter = 'a';  //2 byte
+    Byte a = 1; //1 byte
+    short b = 2; // 2 byte
+    int c = 12; //4 byte
+    long d = 15; // 8 byte
+    float e = 12.4f; //4 byte
+    double f = 23.5; // 8 byte
+     //variables and data types - Non primitive
+     //string 
+     String names = "Asraf";
+     //array
+     int[] no = {1,2,3,45};
+     String[] nameslist = {"Asraf","balaji"};
+     // then classes and objects,interfaces ,collections
+  
+
+    
+
+    }
+
+}
---
+++ Day 2/Demo2.java
@@ -0,0 +1,14 @@
+
+public class Demo2 {
+    public static void main(String[] args) {
+     /* Typecasting */
+     /* Implicit casting  */
+     int a = 34;
+     long b = a; // Automatic casting
+     System.out.println(b);
+    /* Explicit Casting */
+     int f = 56;
+     short d = (short)f;//Manual Casting
+     System.out.println(d);
+    }
+}
