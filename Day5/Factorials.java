97d7a809b32e042959069d3876aa9380dc9558c9
asrafaleem <asrafaleem8@gmail.com>
2025-03-23 16:05:27 +0530

    commit changes

--- Day5/Factorials.java
+++ Day5/Factorials.java
@@ -1,14 +1,13 @@
+package Day5;
 public class Factorials {
-    static public int factorial(int n) {
-        if(n==1)
+    public static int factorial(int n) {
+        if (n == 0 || n == 1)  // Handling base case for 0! and 1!
             return 1;
         else
-            return n*factorial(n-1);
+            return n * factorial(n - 1);
     }
+
     public static void main(String[] args) {
-        System.out.println(factorial(5));
-    
-        
+        System.out.println(factorial(5)); // Output: 120
     }
-    
 }
