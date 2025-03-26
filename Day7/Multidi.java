5c9270147d1f1fd23f29abbe5225e38a4f530a48
asrafaleem <asrafaleem8@gmail.com>
2025-03-26 19:13:23 +0530

    new changes

---
+++ Day7/Multidi.java
@@ -0,0 +1,12 @@
+public class Multidi {
+    public static void main(String[] args){
+        int[][] arr1 = new int[3][3];
+        
+      int[][] A = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}  
+      };
+      System.out.println(A[0][2]);
+}
+}
